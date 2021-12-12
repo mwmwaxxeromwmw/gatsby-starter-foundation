f3e4fddfff16d03e6ebf5841f616159fc6ed29a0
mwmwaxxeromwmw <81531384+mwmwaxxeromwmw@users.noreply.github.com>
2021-12-12 14:51:02 +0200

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,23 +9,8 @@ import Navigation from "./navigation"
 import "../assets/scss/style.scss"
 import Footer from "./footer"
 import Theme from "../components/theme"
-import Search from "../components/search"
-
-const query = graphql`
-  query LayoutQuery {
-    site {
-      siteMetadata {
-        siteTitle: title
-      }
-    }
-    siteSearchIndex {
-      index
-    }
-  }
-`
 
 const Layout = ({ children, className, props }) => {
-  const { site, siteSearchIndex } = useStaticQuery(query)
   const { siteTitle } = site.siteMetadata
 
   return (
@@ -33,15 +18,9 @@ const Layout = ({ children, className, props }) => {
       <Header>
         <Logo title={siteTitle} />
         <div sx={layoutStyle.nav}>
-          <div sx={{ display: ["flex", "flex", "flex", "none"] }}>
-            <Search searchIndex={siteSearchIndex.index} />
-          </div>
           <Navigation />
         </div>
-        <div sx={layoutStyle.appearance}>
-          <Search searchIndex={siteSearchIndex.index} />
           <Theme />
-        </div>
       </Header>
       <main className={"container " + className}>{children}</main>
       <Footer />
