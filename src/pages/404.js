3154de9e1ecd094b67780b88cb4e848631b1634d
mwmwaxxeromwmw <81531384+mwmwaxxeromwmw@users.noreply.github.com>
2022-02-01 12:33:37 +0200

    added RiCarWashingLine to 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import { RiArrowLeftSLine, RiBugLine, RiSkullLine } from "react-icons/ri"
+import { RiArrowLeftSLine, RiBugLine, RiSkullLine, RiCarWashingLine } from "react-icons/ri"
 
 import Seo from "../components/seo"
 import Layout from "../components/layout"
