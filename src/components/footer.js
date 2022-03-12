9edf96b335ed9f6a9b70ef0e37260a2007374573
mwmwaxxeromwmw <81531384+mwmwaxxeromwmw@users.noreply.github.com>
2022-03-12 13:05:28 +0200

    edited footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,6 +12,9 @@ const Footer = () => (
       <p>
       <small>&copy; Copyright 2022, Moks Auto. All Rights Reserved</small>
       </p>
+      <ul class="social_footer_ul">
+        <li><a href="https://www.moksauto.com"><i class="fab fa-envelope"></i></a></li>
+        </ul>
     </div>
   </footer>
 )
