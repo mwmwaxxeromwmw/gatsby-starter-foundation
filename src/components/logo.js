ad415844e01b97b037a9669421b41377e20de17b
mwmwaxxeromwmw <axxeromw@gmail.com>
2026-09-07 00:40:16 +0200

    increase logo height from 50 to 75

--- src/components/logo.js
+++ src/components/logo.js
@@ -9,7 +9,7 @@ const Logo = () => (
         src="../../static/assets/logo.png" 
         alt="Moks Auto Logo"
         placeholder="blurred"
-        height={50} // Adjust this height to fit your header layout
+        height={75} // Adjust this height to fit your header layout
       />
     </Link>
   </div>
