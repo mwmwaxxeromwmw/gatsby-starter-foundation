99b875e11aceb7ad96a0ad17d15203c5916517bc
mwmwaxxeromwmw <axxeromw@gmail.com>
2026-09-07 00:34:04 +0200

    delete search.js & add logo.png and mod logo.js

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,9 +1,16 @@
-import React from "react"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
-const Logo = props => (
+const Logo = () => (
   <div className="site-logo">
-    <Link to="/">{props.title}</Link>
+    <Link to="/">
+      <StaticImage 
+        src="../../static/assets/logo.png" 
+        alt="Moks Auto Logo"
+        placeholder="blurred"
+        height={50} // Adjust this height to fit your header layout
+      />
+    </Link>
   </div>
 )
 
